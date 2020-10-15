594dd2c7246cbbd3b43ccfcabce2456b008baf41
BOUZIANE Mohamed Eldjilali <djmohamed1@gmail.com>
2020-10-15 15:10:46 +0200

    hash tables

---
+++ dictionaries_and_hashmaps/src/hash_tables/Solution.java
@@ -0,0 +1,69 @@
+package hash_tables;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(String[] magazine, String[] note) {
+
+        Hashtable<String, Integer> hashtable
+                = new Hashtable<String, Integer>();
+
+        for (int i = 0; i < magazine.length; i++) {
+            hashtable.merge(magazine[i],1,Integer::sum);
+        }
+
+
+        for(String word:note) {
+            if (hashtable.get(word) == null || hashtable.get(word) == 0) {
+                System.out.println("No");
+                return;
+            }else {
+                hashtable.merge(word, -1, Integer::sum);
+            }
+        }
+
+        System.out.println("Yes");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        String[] magazine = new String[m];
+
+        String[] magazineItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            String magazineItem = magazineItems[i];
+            magazine[i] = magazineItem;
+        }
+
+        String[] note = new String[n];
+
+        String[] noteItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            String noteItem = noteItems[i];
+            note[i] = noteItem;
+        }
+
+        checkMagazine(magazine, note);
+
+        scanner.close();
+    }
+}
