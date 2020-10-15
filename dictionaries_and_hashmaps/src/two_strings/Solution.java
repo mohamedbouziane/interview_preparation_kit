9752881873fc51340be7f4d6663e8cc4f0e40f48
BOUZIANE Mohamed Eldjilali <djmohamed1@gmail.com>
2020-10-15 16:42:30 +0200

    tow string

---
+++ dictionaries_and_hashmaps/src/two_strings/Solution.java
@@ -0,0 +1,59 @@
+package two_strings;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        if (s1 == null || s2 == null) {
+            return "No";
+        }else {
+
+            Map<Character, Integer> map = new HashMap<>();
+
+            for (Character c : s1.toCharArray()) {
+                map.merge(c, 1, Integer::sum);
+            }
+            for (Character c : s2.toCharArray()) {
+                if (map.get(c) != null) {
+                    return "YES";
+                }
+            }
+        }
+
+        return "NO";
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
